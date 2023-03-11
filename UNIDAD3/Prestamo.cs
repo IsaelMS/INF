6ba9768814bac8353aeffa9048e446c933767aa5
Isael Méndez Santana <126363258+IsaelMS@users.noreply.github.com>
2023-03-11 11:07:51 -0400

    Create Prestamo.cs

---
+++ UNIDAD3/Prestamo.cs
@@ -0,0 +1,81 @@
+using System;
+
+public class Prestamo {
+
+    private double cantidad;
+    private int tiempo;
+    private double interes;
+    private string prestamista = "Financiera Mendez";
+    private string prestatario;
+    private double cuotaMensual;
+    ///////////////////////////////////////////////////
+    public  void setCantidad(double cantidad){
+        this.cantidad = cantidad;
+    }
+    public double getCantidad(){
+        return this.cantidad;
+    }
+    ////////////////////////////////////////////////////
+    public void setTiempo(int tiempo){
+        this.tiempo = tiempo;
+    }
+    public int getTiempo(){
+        return this.tiempo;
+    }
+    /////////////////////////////////////////////////////
+    public void setInteres(double interes){
+        this.interes = interes/100;
+        
+    }
+    public double getInteres(){
+        return this.interes;
+    }
+    /////////////////////////////////////////////////////
+    public void setPrestatario(string prestatario){
+        this.prestatario = prestatario;
+    }
+    public string getPrestatario(){
+        return this.prestatario;
+    }
+    /////////////////////////////////////////////////////
+    public void deplegarInformacion(){
+
+        Console.WriteLine("\n\n\t*****Resumen*****\n");
+        Console.WriteLine("Nombre del prestatario: {0}", prestatario);
+        Console.WriteLine("Entidad financiera: {0}", prestamista);
+        Console.WriteLine("Timepo en meses: {0}", tiempo);
+        Console.WriteLine("Cantidad solicitada: {0}", cantidad);
+        Console.WriteLine("Interes total: " + Math.Round((cantidad*interes), 2));
+        cuotaMensual = ((cantidad*(1+interes))/tiempo);
+        cuotaMensual = Math.Round(cuotaMensual, 2);
+        Console.WriteLine("Cuota mensual: {0}", cuotaMensual);
+
+    }
+
+
+}
+
+public class PrestamoTest{
+    static void Main(){
+
+        Prestamo prestamo1 = new Prestamo();
+        Console.Clear();
+
+        Console.WriteLine("Isael Mendez\n100429639\n");
+        string mensaje = "Calculo de prestamo.\nFavor de ingresar la informacion solicitada.";
+
+        Console.WriteLine(mensaje);
+        Console.WriteLine("\nNombre del prestatario: ");
+        prestamo1.setPrestatario(Console.ReadLine());
+        Console.WriteLine("Cantidad: ");
+        prestamo1.setCantidad(double.Parse(Console.ReadLine()));
+        Console.WriteLine("Timepo en meses: ");
+        prestamo1.setTiempo(int.Parse(Console.ReadLine()));
+        Console.WriteLine("Tasa de interes anual: ");
+        prestamo1.setInteres(double.Parse(Console.ReadLine()));
+
+        prestamo1.deplegarInformacion();
+
+    }
+
+}
